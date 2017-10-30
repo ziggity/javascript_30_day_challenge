66cd4ce9af67fbae4bff595ad788fd9081354d5a
zach beecher <zachbeecher@gmail.com>
2017-10-29 19:17:57 -0700

    video speed controller

--- countdown timer/scripts.js
+++ countdown timer/scripts.js
@@ -49,7 +49,6 @@ buttons.forEach(button => button.addEventListener('click', startTimer));
 document.customForm.addEventListener('submit', function(e) {
   e.preventDefault();
   const mins = this.minutes.value;
-  console.log(mins);
   timer(mins * 60);
   this.reset();
 });
